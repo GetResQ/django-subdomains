059ba9f75614f564b982ea58b2105680fba38aab
Edward D'Souza <edsouza+gh@gmail.com>
2020-04-17 13:32:48 -0400

    Pin Django to 2.0.13

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except ImportError:
     version = get_version()
 
 
-install_requires = ['django']
+install_requires = ['django==2.0.13']
 tests_require = install_requires + ['mock']
 
 setup(name='django-subdomains',
